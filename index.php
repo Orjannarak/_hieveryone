ccbbe1542e2f4cc2995d50b5d229b5a343ca6c0b
Orjannarak <97937647+Orjannarak@users.noreply.github.com>
2022-02-09 22:35:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,37 +15,37 @@
         <td>1</td>
         <td>Payton Moormeier</td>
         <td>Payton</td>
-        <td><img src="file:///C:/Users/GIGABYTE/Downloads/payton..jpg" width="100px"></td>
+        <td><img src="https://ae01.alicdn.com/kf/H635da670b87d462aaca81a947ea1f35d2/2020-Payton-Moormeier-Merch-Hoodies-Social-Media-Hoodies-Tshire.jpg" width="100px"></td>
         <td><a href="https://www.instagram.com/paytonmoormeier/?utm_medium=copy_link&fbclid=IwAR1uELGsKqZbTL3SDKl3ZWMElhMzR8FHyFnqNC7CVFAVdhSk1vrUqMD3geo">instagram</a></td>
       <tr>
         <td>2</td>
         <td>Harry Styles</td>
         <td>Harry</td>
-        <td><img src="file:///C:/Users/GIGABYTE/Downloads/harry.jpg" width="100px"></td>
+        <td><img src="https://64.media.tumblr.com/1ca9f033c9c35bc7c05b7c4a7f922288/fbf5c29d8ce1a93d-c6/s540x810/4a8fbc74155c8ab87b29f7cf56a6a5bb43efd534.png" width="100px"></td>
         <td><a href="https://www.instagram.com/harrystyles/?fbclid=IwAR1vs9bLJuJ_FkR3CI1RRz3EaBhmUE7I7FTTbS5yVgKFQ6tWedxOYEzHmow">instagram</a></td>
       <tr>
         <td>3</td>
         <td>Charlie Puth</td>
         <td>Charlie</td>
-        <td><img src="file:///C:/Users/GIGABYTE/Downloads/charlie.jpg" width="100px"></td>
+        <td><img src="https://pbs.twimg.com/media/EtNHfr_VkAAbXsQ.jpg" width="100px"></td>
         <td><a href="https://www.instagram.com/charlieputh/?utm_medium=copy_link&fbclid=IwAR1MKlptFZtr0ZzrBWWnhFIQO0deTYbYbCGPw9WSP-WpP-RQc8hMPIWFlT0">instagram</a></td>
       <tr>
         <td>4</td>
         <td>Christ Hemsworth</td>
         <td>Christ</td>
-        <td><img src="file:///C:/Users/GIGABYTE/Downloads/christ.jpg" width="100px"></td>
+        <td><img src="https://www.ladbible.com/cdn-cgi/image/width=720,quality=70,format=jpeg,fit=pad,dpr=1/https%3A%2F%2Fs3-images.ladbible.com%2Fs3%2Fcontent%2Fcdf213b261b9be159a422957d08302bd.png" width="100px"></td>
         <td><a href="https://www.instagram.com/chrishemsworth/?fbclid=IwAR0arId_Shuus9Y2okt5Pe5icizlUdpTrRY54LLj8X-nicb-Hz1qbe9EerE">instagram</a></td>
       <tr>
         <td>5</td>
         <td>Tom Holland</td>
         <td>Tom Holland</td>
-        <td><img src="file:///C:/Users/GIGABYTE/Downloads/tom.jpg" width="100px"></td>
+        <td><img src="https://decider.com/wp-content/uploads/2019/06/tom-holland-graham-norton.jpg?quality=80&strip=all" width="100px"></td>
         <td><a href="https://www.instagram.com/tomholland2013/?utm_medium=copy_link&fbclid=IwAR3_39WGZ6zg6KNRkIYvYEBCFn_NP3b8opxVE8MaQUcvy-TUv84af4P6hAs">instagram</a></td>
       <tr>
         <td>6</td>
         <td>Robert Downey Jr.</td>
         <td>Robert Downey</td>
-        <td><img src="file:///C:/Users/GIGABYTE/Downloads/robert.jpg" width="100px"></td>
+        <td><img src="https://www.themarysue.com/wp-content/uploads/2018/12/tony-stark-face.jpg" width="100px"></td>
         <td><a href="https://www.instagram.com/robertdowneyjr/?utm_medium=copy_link&fbclid=IwAR3nO8-oCUbmOhu9X8lU5ejsFSRpWH6If3mqJ4YSNfRA5tMe_R_Y3coDcAY">instagram</a></td>
       </tr>
     </table>
