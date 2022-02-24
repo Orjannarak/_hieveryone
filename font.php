4e503ce1ba0a737436bba68f8664d9a92a0b8ad5
Orjannarak <97937647+Orjannarak@users.noreply.github.com>
2022-02-24 11:31:50 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("smile-Regular.woff");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("ChocolateteehaypaiTEST.woff");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">นางสาวกชพรรณ ตุลสุข เลขที่ 14 ห้อง ม.4/13 เลขประจำตัว 57023</h1>
+    <h1 id="font2">นางสาวกชพรรณ ตุลสุข เลขที่ 14 ห้อง ม.4/13 เลขประจำตัว 57023</h1>
+  </body>
+</html>
